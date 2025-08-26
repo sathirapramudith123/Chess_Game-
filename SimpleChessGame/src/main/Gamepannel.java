427180094518a0c99221531ba612fafd32d0af41
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-08-26 20:53:32 +0530

    game loop

--- SimpleChessGame/src/main/Gamepannel.java
+++ SimpleChessGame/src/main/Gamepannel.java
@@ -2,16 +2,40 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class Gamepannel extends JPanel {
+public class Gamepannel extends JPanel implements Runnable{
 
 	public static final int WIDTH = 1010;
 	public static final int HEIGHT = 650;
+	final int FPS = 60;
+	Thread gameThread;
 	
 	public Gamepannel() {
 		setPreferredSize(new Dimension(WIDTH,HEIGHT));
 		setBackground (Color.black);
 	}
+	
+	
+	public void lunchGame () {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	@Override
+	public void run() {
+		
+	}
+	
+	private void update() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
+	
+	
+	
 }
