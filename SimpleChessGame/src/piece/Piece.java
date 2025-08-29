b9f02fae8d91b4e3db9738c25b22d1da680a679b
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-08-29 11:59:19 +0530

    create a piece java

---
+++ SimpleChessGame/src/piece/Piece.java
@@ -0,0 +1,44 @@
+package piece;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import main.Board;
+
+public class Piece {
+	
+	public BufferedImage image;
+	public int x, y;
+	public int col, row, perCol, perRow; //pervieous column and pervious row  
+	public int color; 
+	
+	public Piece(int color, int col, int row) {
+		
+		this.color = color;
+		this.col = col; 
+		this.row = row;
+		x = getX(col);
+		y= getY(row);
+		perCol = col;
+		perRow = row;
+	}
+	
+	public BufferedImage getImage (String imagePath){
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(getClass().getResource(imagePath + ".png"));
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return image;
+	}
+	
+	public int getX(int col) {
+		return col * Board.SQUARE_SIZE;
+	}
+	public int getY(int row) {
+		return row * Board.SQUARE_SIZE;
+	}
+}
