4dd7452091af5de42a914b8965ddc32149ab2a3f
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-08-30 12:23:17 +0530

    add a pawan in a borad

--- SimpleChessGame/src/piece/Pawan.java
+++ SimpleChessGame/src/piece/Pawan.java
@@ -1,10 +1,18 @@
 package piece;
 
+import main.Gamepannel;
+
 public class Pawan extends Piece {
 
 	public Pawan(int color, int col, int row) {
 		super(color, col, row);
 		
+		if(color == Gamepannel.WHITE) {
+			image = getImage("/piece/w-pawan");
+		}
+		else {
+			image = getImage("/piece/b-pawan");
+		}
 	}
 
 	
