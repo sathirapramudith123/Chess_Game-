115a7cd51f53e42c18fc6f54dadba0ae2768f3d4
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-09-08 14:52:56 +0530

    create movement a chess pieces

---
+++ SimpleChessGame/src/main/Mouse.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Mouse extends MouseAdapter {
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		
+	}
+	
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
+	}
+}
