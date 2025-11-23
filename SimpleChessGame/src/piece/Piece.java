d12192b67ee1dad8594a5b29b829d762452572b9
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-11-23 13:12:31 +0530

    update a pieces

--- SimpleChessGame/src/piece/Piece.java
+++ SimpleChessGame/src/piece/Piece.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import main.Board;
+import main.Gamepannel;
 
 public class Piece {
     
@@ -14,6 +15,7 @@ public class Piece {
     public int x, y;
     public int col, row, perCol, perRow; // previous column and row  
     public int color; 
+    public Piece hittingP;
     
     public Piece(int color, int col, int row) {
         this.color = color;
@@ -68,8 +70,23 @@ public class Piece {
     public boolean canMove(int tragetCol, int tragetRow) {
     	return false;
     }
-    public boolean isWithinBoard(int targetCol, int tragetRow) {
-    	if(targetCol >= 0 && targetCol <= 7 && tragetRow >= 0 && tragetRow <= 7) {
+    public boolean isWithinBoard(int targetCol, int targetRow) {
+    	if(targetCol >= 0 && targetCol <= 7 && targetRow >= 0 && targetRow <= 7) {
+    		return true;
+    	}
+    	return false;
+    }
+    public Piece getHittingP(int targetCol, int targetRow) {
+    	for(Piece piece : Gamepannel.simPieces) {
+    		if(piece.col == targetCol && piece.row == targetRow && piece != this) {
+    			return piece;
+    		}
+    	}
+    	return null;
+    }
+    public boolean isVaildSquare(int targetCol, int targetRow) {
+    	hittingP = getHittingP(targetCol, targetRow);
+    	if(hittingP ==null) {
     		return true;
     	}
     	return false;
